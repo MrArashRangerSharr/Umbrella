4e930e92ef139a871f405083ea370b5b040da1e0
unknowntg <arashnomiri@gmail.com>
2016-05-05 19:04:37 +0430

    Create version.lua

---
+++ plugins/version.lua
@@ -0,0 +1,33 @@
+do
+
+function run(msg, matches)
+  return 'Umbrella Telegram Bot v1.5'.. [[ 
+  
+  Website:
+  http://Umbrella.shayan-soft.IR
+  https://avastteam.r98.ir
+  Antispam Bot: @UmbrellaTG
+  
+  Channel:
+  @UmbrellaTeam
+  @avast_Team
+  Sudo (Admin): @shayansoft
+  (This is not an open original source version and this version designed by @avast_Team )
+  Powered by:
+  shayan soft Co. Group
+  Via @avast_Team
+  Engineer Shayan Ahmadi
+  Open Source By @arashnomiri
+  http://shayan-soft.IR]]
+end
+
+return {
+  description = "Robot and Creator About", 
+  usage = "/ver : robot info",
+  patterns = {
+    "^[!/]ver$"
+  }, 
+  run = run 
+}
+
+end
